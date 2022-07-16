82e62f9465e40cc43ef00486c446421cfc73c24f
Ashish <shirsalkarashishprakash@gmail.com>
2022-07-16 17:42:46 +0530

    ready for deploy v1

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: waitress-serve --port=$PORT library_management.wsgi:application
--- library_management/settings.py
+++ library_management/settings.py
@@ -18,14 +18,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
-
+import os
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3llng6fjitzxpq)-8gqpx%b&78wj6psfcccd4d2#vgzk4@n4(3'
+SECRET_KEY = os.environ['SECRET_KEY']
+#SECRET_KEY = 'django-insecure-3llng6fjitzxpq)-8gqpx%b&78wj6psfcccd4d2#vgzk4@n4(3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["library--management-webapp.herokuapp.com", "localhost"]
 
 
 # Application definition
@@ -43,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -118,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR/'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
